798441257d57ca61a66b52831351177310b6888e
zainsci <60652827+zainsci@users.noreply.github.com>
2021-12-13 22:32:49 +0500

    UPDATE: Add Day 2020/2

---
+++ 2020/2/02.py
@@ -0,0 +1,26 @@
+from collections import defaultdict
+
+
+def solve(data):
+    count = 0
+
+    for line in data:
+        nums, letter, password = line.split(" ")
+        low, high = map(int, nums.split("-"))
+        letter = letter[0]
+
+        if bool(password[low - 1] == letter) != bool(password[high - 1] == letter):
+            count += 1
+
+    return count
+
+
+def main():
+    with open("input") as f:
+        data = f.read().strip().split("\n")
+
+        print(solve(data))
+
+
+if __name__ == "__main__":
+    main()
